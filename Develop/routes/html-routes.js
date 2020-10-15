9675eb9015fc1c3276dad33e4d7b529a5cccfe18
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-10-15 18:27:57 -0400

    added the code from the starter pack + the ai and htm route files

---
+++ Develop/routes/html-routes.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const router = require("express").Router();
+
+  router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+  router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+  module.exports = router;
