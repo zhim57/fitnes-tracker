89c207c81469df10883d3bae3e6042c70ab17e44
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-10-16 09:03:35 -0400

    setting up for deployment on heroku

--- Develop/server.js
+++ Develop/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(logger("dev"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongodb", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
