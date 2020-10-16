bffb0ff430302f6b633653e3a7b8afdd6176a786
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-10-16 09:43:21 -0400

    fixed directories

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(logger("dev"))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongodb", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+// routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});
